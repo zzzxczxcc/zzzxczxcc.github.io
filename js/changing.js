de4ad56ef681e17f640b1123ba4ed04ab2dfd3a1
zzzxczxcc <kypsuk@gmail.com>
2025-04-09 16:54:39 +0300

    Delete js directory

--- js/changing.js
+++
@@ -1,3 +0,0 @@
-//to add more titles do ,"yourtitle" etc.
-const rrr=["private biolink","invite only","knives.rip"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
-setInterval(yyy,250)
